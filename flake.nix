77fcc060c2e7945e721f848d758974b48bb656a2
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-12 06:40:13 +0200

    chore: refresh flake inputs and remove pycache artifacts

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "anx: article toolchain — figure layout, generation, and manuscript building";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "git+https://codefloe.com/caniko/rs-harbor.git?ref=trunk&rev=7fa1c2104dab4e1dbaa1aaa6df84bba815aa282d";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     tex-harbor = {
       url = "git+https://codeberg.org/caniko/tex-harbor.git?ref=trunk";
