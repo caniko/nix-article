f984ad6ced0b19f61647a92f91dd669879268157
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:35:46 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "anx: article toolchain — figure layout, generation, and manuscript building";
 
   inputs = {
-    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     tex-harbor = {
       url = "git+https://codeberg.org/caniko/tex-harbor.git?ref=trunk";
